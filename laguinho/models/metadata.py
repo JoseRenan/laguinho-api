e9800f1e3a17df73b47b36e675fe4cf9a086be07
paulojbleitao <paulojbleitao@gmail.com>
2019-11-19 14:38:57 -0300

    Adiciona a fonte aos metadados dos datasets

--- laguinho/models/metadata.py
+++ laguinho/models/metadata.py
@@ -6,6 +6,8 @@ class DatasetMetadataSchema(Schema):
     name = fields.Str(required=True, error_messages=get_required_error("name"))
     url = fields.Str(required=True, error_messages=get_required_error("url"))
     path = fields.Str(required=True, error_messages=get_required_error("path"))
+    source = fields.Str(required=True,
+                        error_messages=get_required_error("source"))
     maintainers = fields.List(fields.Str())
     format = fields.Str(validate=validate.OneOf(["json", "csv"]),
                         required=True,
