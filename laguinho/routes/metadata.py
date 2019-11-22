fa4d6a25cd9363ed4e9b714ff0e76c5ae2513bf8
paulojbleitao <paulojbleitao@gmail.com>
2019-11-22 16:09:04 -0300

    Renomeia rota 'datasets' para 'metadata'

--- laguinho/routes/metadata.py
+++ laguinho/routes/metadata.py
@@ -3,12 +3,12 @@ from laguinho.models.metadata import dataset_metadata
 from marshmallow import ValidationError, EXCLUDE
 from laguinho.extensions import mongo
 
-datasets = Blueprint('datasets', __name__)
-datasets_metadata = mongo.db.datasets
+metadata = Blueprint('metadata', __name__)
+metadata_db = mongo.db.metadata
 
 
 def dataset_exists(dataset):
-    return datasets_metadata.find_one({
+    return metadata_db.find_one({
         '$or': [{
             'name': dataset['name']
         }, {
@@ -18,27 +18,27 @@ def dataset_exists(dataset):
     })
 
 
-@datasets.route("/datasets", methods=['POST'], strict_slashes=False)
+@metadata.route("/metadata", methods=['POST'], strict_slashes=False)
 def publish():
     result = dataset_metadata.load(request.json, unknown=EXCLUDE)
     if dataset_exists(result):
         return jsonify('Dataset already exists'), 409
-    datasets_metadata.insert_one(result)
+    metadata_db.insert_one(result)
     del result['_id']
     return jsonify(result), 201
 
 
-@datasets.route("/datasets", methods=['GET'], strict_slashes=False)
-def get_datasets():
-    dsets = list(datasets_metadata.find())
+@metadata.route("/metadata", methods=['GET'], strict_slashes=False)
+def get_datasets_metadata():
+    dsets = list(metadata_db.find())
     for ds in dsets:
         del ds['_id']
 
     return jsonify(dsets)
 
 
-@datasets.route("/datasets/<name>", methods=['GET'], strict_slashes=False)
+@metadata.route("/metadata/<name>", methods=['GET'], strict_slashes=False)
 def get_datasets_by_name(name):
-    search_result = datasets_metadata.find_one_or_404({'name': name})
+    search_result = metadata_db.find_one_or_404({'name': name})
     del search_result['_id']
     return jsonify(search_result), 200
