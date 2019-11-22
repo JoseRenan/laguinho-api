1b3db6181c01c983b3ee8e6f340fd23d28a2f5a8
paulojbleitao <paulojbleitao@gmail.com>
2019-11-22 16:39:05 -0300

    Ajusta o schema de microservices para permitir urls em vez de apenas o ip

--- laguinho/models/microservice.py
+++ laguinho/models/microservice.py
@@ -4,10 +4,8 @@ from .error_messages import get_required_error
 
 class MicroserviceMetadataSchema(Schema):
     name = fields.Str(required=True, error_messages=get_required_error('name'))
-    ip = fields.Str(
-        required=True,
-        error_messages=get_required_error('ip'),
-        validate=validate.Regexp('^(?:[0-9]{1,3}\\.){3}[0-9]{1,3}$'))
+    baseURL = fields.Str(required=True,
+                         error_messages=get_required_error('baseURL'))
 
 
 microservice_metadata = MicroserviceMetadataSchema()
