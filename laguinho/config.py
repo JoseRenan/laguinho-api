ada45322459c0ae7caed425816a0da75b3541d66
José Renan <joserenansl99@gmail.com>
2019-11-22 23:58:59 -0300

    Refact config file

--- laguinho/config.py
+++ laguinho/config.py
@@ -2,8 +2,7 @@ import os
 
 
 class BaseConfig:
-    MONGO_HOST = os.getenv('LAGUINHO_MONGO_HOST', default='localhost')
-    MONGO_PORT = os.getenv('LAGUINHO_MONGO_PORT', default='27017')
+    MONGO_HOST_PORT = os.getenv('LAGUINHO_MONGO_HOST_PORT', default='localhost:27017')
     MONGO_DBNAME = os.getenv('LAGUINHO_MONGO_DBNAME', default='laguinho')
     MONGO_USERNAME = os.getenv('LAGUINHO_MONGO_USERNAME',
                                default='opendevufcg')
@@ -11,4 +10,4 @@ class BaseConfig:
                                default='laguinhoapi')
     MONGO_AUTH_SOURCE = os.getenv('LAGUINHO_MONGO_AUTH_SOURCE',
                                default='admin')
-    MONGO_URI = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST}:{MONGO_PORT}/{MONGO_DBNAME}?authSource={MONGO_AUTH_SOURCE}'
+    MONGO_URI = f'mongodb://{MONGO_USERNAME}:{MONGO_PASSWORD}@{MONGO_HOST_PORT}/{MONGO_DBNAME}?authSource={MONGO_AUTH_SOURCE}'
